b9825e241a46fb927bad7bc9d43ed260cfa66de1
Victor Maia Aldecoa <victor.maia.a@gmail.com>
2013-02-28 16:50:18 +0100

    Added new classes

---
+++ WSSemanticMatching/src/wssemanticmatching/ontology/OntologyResult.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wssemanticmatching.ontology;
+
+/**
+ *
+ * @author victor
+ */
+public enum OntologyResult {
+    Exact,
+    PlugIn,
+    Subsumption,
+    Structural,
+    NotMatched
+}
